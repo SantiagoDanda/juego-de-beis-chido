de142a5c89807e658473b23e50e6fdf032198a47
SantiagoDanda <321146650@alumno.enp.unam.mx>
2023-01-21 22:37:12 -0600

    Ahora el fichero se abre hasta donde necesitamos

--- juego.c
+++ juego.c
@@ -32,13 +32,32 @@ void menu()
 
     WINDOW* vntntxt = crear_ventana(5, 100, 0, 0);
     refresh();
-    mvwin(vntntxt, 0, COLS/2-50);
+    mvwin(vntntxt, 5, COLS/2-50); //mueve la ventana del texto al centro
 
-    char cadena[500]; 
+    char crctr;
+    int i = 0; 
     while(!feof(texto)){
-        fgets(cadena, 100, texto);
-        wprintw(vntntxt, "%s", cadena);
+        crctr = fgetc(texto);
+        if(((char)crctr) == '1') //Si el caracter encontrado en el fichero es "1", deja de leerse
+            break;
+        wprintw(vntntxt, "%c", crctr);
     }
     wrefresh(vntntxt);
     fclose(texto);
+
+    WINDOW* vntnopciones = crear_ventana(12, 15, 15, COLS/2-7);
+    refresh();
+
+    //wattron(vntnopciones, A_BOLD || A_ITALIC); HACER QUE SIRVA
+    mvwprintw(vntnopciones, 2, 5, "Jugar");
+    mvwprintw(vntnopciones, 4, 1, "Instrucciones");
+    mvwprintw(vntnopciones, 6, 2, "Puntuaciones");
+    mvwprintw(vntnopciones, 8, 4, "Créditos");
+    wrefresh(vntnopciones);
+    /*
+    Jugar
+    Instrucciones
+    Puntuaciones
+    Créditos
+    */
 }
